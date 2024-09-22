6e7ed0c11059f401c9e6b70c2e38cb90a7cad0aa
developersview <pcslg1998@gmail.com>
2024-09-23 01:53:02 +0530

    updated .gitignore and added powershell script

---
+++ run_api.ps1
@@ -0,0 +1,20 @@
+# run_fastapi.ps1
+
+# Define paths
+$projectPath = "D:\_DA+DS\03_Python\business_api_python"
+$venvPath = "$projectPath\myenv"
+$scriptPath = "$projectPath\main.py"
+
+# Navigate to the project directory
+Set-Location -Path $projectPath
+
+try {
+    . "$venvPath\Scripts\Activate.ps1"
+    #pip install -r requirements.txt
+    uvicorn main:app --host 192.168.0.106 --port 8000 --reload
+	#uvicorn main:app --reload
+
+}
+catch {
+    Write-Error "An error occurred: $_"
+}
